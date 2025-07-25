d10d26ed49b6983a81b5779b29e4015f2e2c3d69
luck-git <abhig1423@gmail.com>
2025-07-26 01:09:41 +0530

    new changes

--- terraform/modules/codepipeline/variable.tf
+++ terraform/modules/codepipeline/variable.tf
@@ -7,10 +7,27 @@ variable "environment" {
   type        = string
   description = "Environment (dev/stage/prod)"
 }
-variable "github_owner"   { type = string }
-variable "github_repo"    { type = string }
-variable "github_token"   { type = string }
-variable "github_branch"  { type = string }
+variable "github_owner" {
+  description = "GitHub repository owner"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_branch" {
+  description = "GitHub branch name"
+  type        = string
+  default     = "main"
+}
 
 variable "tags" {
   type        = map(string)
