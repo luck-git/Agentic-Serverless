2a61d8e5df17440c36f7b1ea997a456d85852036
luck-git <abhig1423@gmail.com>
2025-07-26 01:47:10 +0530

    added new policy

--- terraform/modules/codepipeline/iamroles.tf
+++ terraform/modules/codepipeline/iamroles.tf
@@ -277,3 +277,27 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
     ]
   })
 }
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name = "codebuild-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.codepipeline_artifacts.arn,
+          "${aws_s3_bucket.codepipeline_artifacts.arn}/*"
+        ]
+      }
+    ]
+  })
+}
