43e0306ae3a6cf06ab7e2ab9f2710b2aa31c5132
luck-git <abhig1423@gmail.com>
2025-07-26 01:35:57 +0530

    added new policies for codebuild

--- terraform/modules/codepipeline/iamroles.tf
+++ terraform/modules/codepipeline/iamroles.tf
@@ -42,16 +42,33 @@ resource "aws_iam_role_policy" "codebuild_basic_policy" {
         Action = [
           "s3:GetObject",
           "s3:GetObjectVersion",
-          "s3:PutObject"
+          "s3:PutObject",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          "s3:GetBucketEncryption",
+          "s3:PutBucketEncryption",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging",
+          "s3:GetBucketLogging",
+          "s3:PutBucketLogging"
         ]
         Resource = [
-          "${aws_s3_bucket.codepipeline_artifacts.arn}/*"
+          "${aws_s3_bucket.codepipeline_artifacts.arn}/*",
+          aws_s3_bucket.codepipeline_artifacts.arn
         ]
       },
       {
         Effect = "Allow"
         Action = [
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketLocation"
         ]
         Resource = aws_s3_bucket.codepipeline_artifacts.arn
       }
@@ -91,6 +108,11 @@ resource "aws_iam_role_policy" "codebuild_terraform_policy" {
           "iam:PassRole",
           "iam:TagRole",
           "iam:UntagRole",
+          "iam:CreatePolicy",
+          "iam:DeletePolicy",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions",
           
           # Lambda permissions
           "lambda:*",
@@ -105,19 +127,94 @@ resource "aws_iam_role_policy" "codebuild_terraform_policy" {
           "cloudwatch:*",
           "logs:*",
           
-          # S3 permissions for state backend
+          # CodeBuild permissions - THIS WAS MISSING
+          "codebuild:BatchGetProjects",
+          "codebuild:CreateProject",
+          "codebuild:UpdateProject",
+          "codebuild:DeleteProject",
+          "codebuild:ListProjects",
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds",
+          
+          # CodePipeline permissions - THESE WERE MISSING
+          "codepipeline:CreatePipeline",
+          "codepipeline:UpdatePipeline",
+          "codepipeline:DeletePipeline",
+          "codepipeline:GetPipeline",
+          "codepipeline:GetPipelineState",
+          "codepipeline:ListPipelines",
+          "codepipeline:StartPipelineExecution",
+          "codepipeline:StopPipelineExecution",
+          "codepipeline:GetPipelineExecution",
+          "codepipeline:ListPipelineExecutions",
+          "codepipeline:PutWebhook",
+          "codepipeline:DeleteWebhook",
+          "codepipeline:RegisterWebhookWithThirdParty",
+          "codepipeline:DeregisterWebhookWithThirdParty",
+          "codepipeline:ListWebhooks",
+          
+          # S3 permissions for state backend and general bucket operations
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
           "s3:ListBucket",
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          
+          # SNS permissions
+          "sns:CreateTopic",
+          "sns:DeleteTopic",
+          "sns:GetTopicAttributes",
+          "sns:SetTopicAttributes",
+          "sns:Subscribe",
+          "sns:Unsubscribe",
+          "sns:ListSubscriptionsByTopic",
+          "sns:TagResource",
+          "sns:UntagResource",
           
           # Additional permissions
           "ec2:DescribeAvailabilityZones",
           "ec2:DescribeVpcs",
           "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups"
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AttachNetworkInterface",
+          "ec2:DetachNetworkInterface"
         ]
         Resource = "*"
+      }
+    ]
+  })
+}
+
+# CodeBuild SSM Access Policy - FIXED THE PARAMETER NAMES
+resource "aws_iam_role_policy" "codebuild_ssm_access" {
+  name = "${var.project_name}-${var.environment}-codebuild-ssm-access"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath"
+        ]
+        Resource = [
+          "arn:aws:ssm:*:*:parameter/github_token",
+          "arn:aws:ssm:*:*:parameter/github_owner",
+          "arn:aws:ssm:*:*:parameter/github_repo",
+          "arn:aws:ssm:*:*:parameter/github_branch"
+        ]
       }
     ]
   })
@@ -144,6 +241,7 @@ resource "aws_iam_role" "codepipeline_role" {
     Name = "${var.project_name}-${var.environment}-codepipeline-role"
   })
 }
+
 # CodePipeline IAM Policy
 resource "aws_iam_role_policy" "codepipeline_policy" {
   name = "${var.project_name}-${var.environment}-codepipeline-policy"
@@ -175,29 +273,6 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
           "codebuild:StartBuild"
         ]
         Resource = aws_codebuild_project.terraform_build.arn
-      }
-    ]
-  })
-}
-resource "aws_iam_role_policy" "codebuild_ssm_access" {
-  name = "codebuild-ssm-parameter-access"
-  role = "agentic-serverless-prod-codebuild-role"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:GetParametersByPath"
-        ]
-        Resource = [
-          "arn:aws:ssm:ap-southeast-2:950916207122:parameter/github_token",
-          "arn:aws:ssm:ap-southeast-2:950916207122:parameter/github_user",
-          "arn:aws:ssm:ap-southeast-2:950916207122:parameter/github_branch"
-        ]
       }
     ]
   })
